410f5ea1793290f049193ee05ee43a7933616b2c
LiwaaHosh <66652532+LiwaaCoder@users.noreply.github.com>
2024-06-20 11:52:57 +0300

    initial graPHql

---
+++ src/test/java/com/example/businessunitsmicroservice/BusinessUnitsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.businessunitsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessUnitsMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
